1ad12b59402c0917fbcc02e3f85fb39ab981e8c0
Seiei Higa <hanachin@gmail.com>
2014-04-17 00:19:41 +0900

    require all Konjak classes in konjak.rb

--- lib/konjak.rb
+++ lib/konjak.rb
@@ -1,5 +1,34 @@
-require "konjak/version"
+require 'konjak/version'
+
+# not elements
+require 'konjak/code_data'
+require 'konjak/text'
+
+# elements
+require 'konjak/element'
+require 'konjak/inline_element'
+require 'konjak/structural_element'
+
+# structural elements
+require 'konjak/body'
+require 'konjak/header'
+require 'konjak/map'
+require 'konjak/note'
+require 'konjak/property'
+require 'konjak/segment'
+require 'konjak/tmx'
+require 'konjak/translation_unit'
+require 'konjak/translation_unit_variant'
+require 'konjak/user_defined_encoding'
+
+# inline elements
+require 'konjak/begin_paired_tag'
+require 'konjak/end_paired_tag'
+require 'konjak/highlight'
+require 'konjak/isolated_tag'
+require 'konjak/placeholder'
+require 'konjak/sub_flow'
+require 'konjak/unknown_tag'
 
 module Konjak
-  # Your code goes here...
 end
