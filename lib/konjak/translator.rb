fde37a2db5014576388ac330d8ce64ad39140a66
Seiei Miyagi <hanachin@gmail.com>
2015-05-19 00:14:03 +0900

    Use translate_env to switch translate strategy

--- lib/konjak/translator.rb
+++ lib/konjak/translator.rb
@@ -5,16 +5,15 @@ require 'konjak/translator/translated_string'
 module Konjak
   class Translator
 
-    using TextTranslate
-
     include Mem
 
-    attr_reader :tmx, :src_lang, :target_lang
+    attr_reader :tmx, :src_lang, :target_lang, :options
 
-    def initialize(tmx, src_lang, target_lang)
-      @tmx = tmx
-      @src_lang = src_lang
+    def initialize(tmx, src_lang, target_lang, **options)
+      @tmx         = tmx
+      @src_lang    = src_lang
       @target_lang = target_lang
+      @options     = options
     end
 
     def translate(doc)
@@ -23,13 +22,30 @@ module Konjak
         translated_docs.map! { |text|
           next text if text.is_a?(TranslatedString)
 
-          tu.translate(src_lang, target_lang, text)
+          env = translate_env.dup
+          env.local_variable_set(:tu, tu)
+          env.local_variable_set(:src_lang, src_lang)
+          env.local_variable_set(:target_lang, target_lang)
+          env.local_variable_set(:text, text)
+          eval('tu.translate(src_lang, target_lang, text)', env)
         }.flatten!
       end
       translated_docs.join
     end
 
     private
+
+    TRANSLATE_ENVS= {
+      text:     Class.new { using TextTranslate; break binding }
+    }
+
+    def format
+      options[:format] || :text
+    end
+
+    def translate_env
+      TRANSLATE_ENVS[format]
+    end
 
     def translation_units
       tmx.body.translation_units.select { |tu|
