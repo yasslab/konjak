857760480572198d455fb1cdedc83163000abd9b
Seiei Miyagi <hanachin@gmail.com>
2015-06-24 18:40:49 +0900

    Replace \u00A0 -> \u0020 before parse gtt tmx
    GTTのtmxはsegmentのleading spaceをnon-breaking spaceとして記録しているよ
    うだった。本来はseg要素のtext nodeのみを置き換えるべきだが、
    通常の文章でnon-breaking spaceが出てくることはなさそうなので、
    XMLをパースする前に置き換えてしまう。

--- lib/konjak/parser.rb
+++ lib/konjak/parser.rb
@@ -10,8 +10,10 @@ module Konjak
 
     def parse(xml)
       if gtt
-        # FIXME
+        # FIXME GTT's tmx store &copy; as &amp;copy;
         xml = xml.gsub(/&amp;(#\d+|#x[0-9a-fA-F]+|[0-9a-zA-Z]+);/) { "&#{$1};" }
+        # FIXME GTT's tmx store leading space(\u0020) as non-breaking space(\u00A0)
+        xml = xml.gsub("\u00A0", ' ')
       end
       doc = Nokogiri::XML.parse(xml)
       Tmx.new doc
